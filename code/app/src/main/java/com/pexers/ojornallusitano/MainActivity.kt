a0be3c9e0100f8c64d83f96cca1c4e3b820f3357
Pexers <pedro-rodri@outlook.com>
2022-11-16 23:48:55 +0000

    Add NavigationView toggle

--- code/app/src/main/java/com/pexers/ojornallusitano/MainActivity.kt
+++ code/app/src/main/java/com/pexers/ojornallusitano/MainActivity.kt
@@ -5,7 +5,9 @@
 package com.pexers.ojornallusitano
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.pexers.ojornallusitano.databinding.ActivityMainBinding
 import com.pexers.ojornallusitano.util.SharedPreferencesData
 
@@ -19,6 +21,14 @@ class MainActivity : AppCompatActivity() {
         SharedPreferencesData.init(applicationContext)  // Load shared preferences
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setSupportActionBar(findViewById(R.id.toolbar_main))
+        val toolbar = binding.toolbarMain.toolbarMain
+        setSupportActionBar(toolbar)
+        initNavToggle(toolbar)
+    }
+
+    private fun initNavToggle(toolbar: Toolbar) {
+        ActionBarDrawerToggle(
+            this, binding.drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close
+        ).syncState()
     }
 }
